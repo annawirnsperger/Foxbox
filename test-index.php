7baaf0c787fab04a3bda573e065a0b079435776e
annawirnsperger <anna@wirnsperger.com>
2016-11-22 19:35:19 +0100

    Tests zu OnePager mit Navbar, Aktueller Stand in test-index festgehalten

---
+++ test-index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>FoxBox</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+    <link href="test-template.css" rel="stylesheet">
+</head>
+<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+
+    <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">FoxBox</a>
+            </div>
+            <div id="navbar" class="navbar-collapse collapse">
+                <ul class="nav navbar-nav">
+                    <li class="page-scroll"><a href="#">Home</a></li>
+                    <li class="page-scroll"><a href="#Über FoxBox">About</a></li>
+                    <li class="page-scroll"><a href="#Registrieren">Contact</a></li>
+                </ul>
+            </div><!--/.nav-collapse -->
+        </div>
+    </nav>
+
+    <section id="Home" class="home-section">
+        Hier ist die Home Seite
+    </section>
+
+    <section id="Über FoxBox" class="über-section">
+        Hier ist die Über Seite
+    </section>
+
+    <section id="Registrieren" class="registrieren-section">
+        Hier ist die Registrieren Seite
+    </section>
+
+
+    - jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+    <script src="test-scrolling-nav.js"></script>
+</body>
+</html>
+
+<!-- Aktueller Stand: Anna 22.11. 20 Uhr: Tutorial 11 min; jQuery Easing plugin fehlt + auf Bootstrap+js lokal zugreifen?-->
